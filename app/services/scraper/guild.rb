5c8bc432a67eedc081f4c3ceebc518398863b3ee
Patricio Sard <patriciosard@gmail.com>
2020-08-20 18:11:43 -0400

    Reduce the minimum scraped pages to 5 and improve constant name

--- app/services/scraper/guild.rb
+++ app/services/scraper/guild.rb
@@ -3,11 +3,11 @@
 module Scraper
   class Guild
     def call
-      page = 1
+      current_page = 1
 
       loop do
-        Logg.info("Requesting #{RUQQUS_GUILDS_URL}&page=#{page}")
-        parsed_body = Nokogiri::HTML API::Request.new("#{RUQQUS_GUILDS_URL}&page=#{page}", 'get').call
+        Logg.info("Requesting #{RUQQUS_SCRAPING_GUILDS_URL}&page=#{current_page}")
+        parsed_body = Nokogiri::HTML API::Request.new("#{RUQQUS_SCRAPING_GUILDS_URL}&page=#{current_page}", 'get').call
 
         guild_cards = parsed_body.css('.card.h-100').css('.card-title').css('a')
 
@@ -16,14 +16,14 @@ module Scraper
         guild_cards.each do |guild|
           guild_name = CGI.escape guild['href'].split('+').last.strip
 
-          guild = ::Guild.find_by('LOWER(name) = LOWER(?)', guild_name)
+          found_guild = ::Guild.find_by('LOWER(name) = LOWER(?)', guild_name)
 
-          return Logg.info('Stopping scrapper since the last guild was found') if guild && (page > 100)
+          return Logg.info('Stopping scrapper since the last guild was found and the minimun quota of pages were scraped') if found_guild && (current_page > MINIMUM_SCRAPED_PAGES)
 
           ::Guild.create!(name: guild_name)
         end
 
-        page += 1
+        current_page += 1
       end
     rescue StandardError => e
       Logg.error(e)
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,5 @@
+RUQQUS_SCRAPING_GUILDS_URL = 'https://ruqqus.com/browse?sort=new'
 RUQQUS_GUILDS_BASE_URL = 'https://ruqqus.com/+'
-RUQQUS_GUILDS_URL = 'https://ruqqus.com/browse?sort=new'
 RUQQUS_API_URL = 'https://ruqqus.com/api/v1'
+
+MINIMUM_SCRAPED_PAGES = 5
