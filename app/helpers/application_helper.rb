1234c7c9436c736e70e191af27ae42b08a9ea1b4
Patricio Sard <patriciosard@gmail.com>
2020-09-19 18:28:43 -0300

    DRY views (add helper methods)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def display_boolean(value)
+    if value
+      content_tag(:span, 'T', class: 'badge badge-success')
+    else
+      content_tag(:span, 'F', class: 'badge badge-danger')
+    end
+  end
+
+  def display_number(number)
+    if number.positive?
+      content_tag(:span, "+#{number_with_delimiter number}", class: 'badge badge-success')
+    elsif number.zero?
+      content_tag(:span, 0, class: 'badge badge-secondary')
+    else
+      content_tag(:span, "-#{number_with_delimiter number}", class: 'badge badge-danger')
+    end
+  end
 end
